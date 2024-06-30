34867edbdb65f1c8ce8ab34adacf322254232a19
NgocDuy1234 <tngo0031@student.monash.edu>
2024-06-30 14:05:13 +1000

    add .env file and dotenv package

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const colors = require("colors");
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
 
+//configure env
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!<h1>");
 })
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`.yellow.bold);
+    console.log(`Server is running on mode ${process.env.DEV_MODE} on port ${PORT}`.yellow.bold);
 })
