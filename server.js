af2b1d61d0520e611523fd1e2ea4a060c09f17d9
NgocDuy1234 <tngo0031@student.monash.edu>
2024-07-02 23:40:35 +1000

    set up routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-
+import authRoute from "./routes/authRoute.js";
 //configure env
 dotenv.config();
 // database config
@@ -12,6 +12,9 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRoute);
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!<h1>");
