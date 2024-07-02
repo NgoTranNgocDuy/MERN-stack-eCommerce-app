87466827b1f6f745a3033398da6054faa6a8ca21
NgocDuy1234 <tngo0031@student.monash.edu>
2024-07-02 23:23:51 +1000

    Set up database for the project and added the connection string to the .env file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //configure env
 dotenv.config();
+// database config
+connectDB();
 const app = express();
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!<h1>");
